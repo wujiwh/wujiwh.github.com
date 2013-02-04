6b26a3e52a8e81651d2cafb6080b5faaac459a7d
wujiwh <wujiwh@gmail.com>
2013-02-04 10:19:36 +0800

    update my vim wiki

---
+++ js/tools.js
@@ -0,0 +1,15 @@
+
+//百度统计
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://"); document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6bad7b5881000f711c6b1e1e6d286a52' type='text/javascript'%3E%3C/script%3E")); 
+
+//多说评论
+	var duoshuoQuery = {short_name:"wangheng"};
+	(function() {
+		var ds = document.createElement('script');
+		ds.type = 'text/javascript';ds.async = true;
+		ds.src = 'http://static.duoshuo.com/embed.js';
+		ds.charset = 'UTF-8';
+		(document.getElementsByTagName('head')[0] 
+		|| document.getElementsByTagName('body')[0]).appendChild(ds);
+	})();
+
